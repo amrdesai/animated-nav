562a745cbe774355dfbbdf8f1ea90f4e334f27f8
Aamer Desai <56724460+minm333@users.noreply.github.com>
2021-01-05 22:56:17 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Variables
+const toggle = document.getElementById('toggle');
+const nav = document.getElementById('nav');
+
+// Event Listener: Nav icon click
+toggle.addEventListener('click', () => nav.classList.toggle('active'));
